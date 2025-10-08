408c5b889fed08419efb6777fa883959f2b21ae9
Lesiamon <koilalesiamon@gmail.com>
2025-10-08 22:10:07 +0300

    file handling deletion on os and appending and insertion

--- filehandling.py
+++ filehandling.py
@@ -1,5 +1,20 @@
-f = open('simple.txt', 'rt')
+#f = open('simple.txt', 'rt')
 #print(f.read())
 #f.close()
-for i in f:
-    print(i)
+#for i in f:
+  #  print(i)
+#this one is for appending a file
+'''
+with open('simple.txt', 'a') as f:
+    f.write("the file contais lesi as a user of some domanin system")
+    '''
+#creating a file in is by using 'x' mode
+'''
+with open('lesi.txt', 'x'):
+    pass
+    '''
+#to dlete a file you must import the os module and reomve .os 
+'''
+import os
+os.remove('lesi.txt')
+'''
